b6a8e121e8259215cfecae494163fb23bfe0b362
NandhiniRavi01 <113678413+NandhiniRavi01@users.noreply.github.com>
2025-03-12 17:07:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -138,4 +138,4 @@ def download_file(filename):
     return send_from_directory(UPLOAD_DIRECTORY, filename,as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5000)
+    app.run(debug=True,host='0.0.0.0',port=5000)
