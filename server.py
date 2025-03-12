0e2a2768da78e975c52d4fc03ce6406057ba40b6
NandhiniRavi01 <113678413+NandhiniRavi01@users.noreply.github.com>
2025-03-12 17:07:21 +0530

    Update server.py

--- server.py
+++ server.py
@@ -47,4 +47,4 @@ def protected():
     return jsonify({"message": f"Welcome {current_user}!"})
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5001)
+    app.run(debug=True,host='0.0.0.0',port=5001)
